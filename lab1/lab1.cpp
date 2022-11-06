c09f7a4d9191ff9ef8ec8be4eb41b4b31d5bb36d
Ihnatenko Vitalii <Ihnatenko Vitalii@DESKTOP-DGTQ2P4>
2022-11-06 20:25:52 +0200

    Добавьте файлы проекта.

---
+++ lab1/lab1.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Enter a number: ";
+    int a;
+    cin >> a;
+    cout << "You entered: " << a << endl;
+    return 0;
+    
+}
